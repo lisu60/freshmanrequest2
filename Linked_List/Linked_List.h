2de5677de354920e81e93408b2246e4e48540329
Taylor <lisu60g@gmail.com>
2013-11-04 23:15:26 +0800

    edited the comment

--- Linked_List/Linked_List.h
+++ Linked_List/Linked_List.h
@@ -8,8 +8,8 @@ struct node
 
 node* create();// return: reference to the first node of the newly-created list
 void remove(int index);// arg: the zero-based index of the node to remove
-node* insert(int index);// arg: the zero-based index of the node to insert. return: reference to the inserted node, NULL if arg>length
+node* insert(int index);// arg: the zero-based index of the node to insert.// return: reference to the inserted node, NULL if arg>length
 int insertToEnd();// return: zero-based index of the inserted node
-node* find(int index);// arg: zero-based index of the node to fine. return: the reference to the found node, NULL if not found
+node* find(int index);// arg: zero-based index of the node to fine.// return: the reference to the found node, NULL if not found
 
 #endif
