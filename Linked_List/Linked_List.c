b0f920cda533d7f2936bf42458a44d0300c24c92
Taylor <lisu60g@gmail.com>
2013-11-04 21:56:10 +0800

    init the repo

---
+++ Linked_List/Linked_List.c
@@ -0,0 +1,13 @@
+#include"Linked_List.h"
+#include<stdlib.h>
+int i;
+
+node* create()
+{
+	return malloc(sizeof(node));
+}
+
+void remove(int index)
+{
+	
+}
---
+++ Linked_List/Linked_List.h
@@ -0,0 +1,15 @@
+#ifndef _LKLIST
+#define _LKLIST
+struct node
+{
+	void* data;
+	node* next;
+}
+
+node* create();// return: reference to the first node of the newly-created list
+void remove(int index);// arg: the zero-based index of the node to remove
+node* insert(int index);// arg: the zero-based index of the node to insert. return: reference to the inserted node, NULL if arg>length
+int insertToEnd();// return: zero-based index of the inserted node
+node* find(int index);// arg: zero-based index of the node to fine. return: the reference to the found node, NULL if not found
+
+#endif
